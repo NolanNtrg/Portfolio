6e0c3380cc2f37e76aee7dc77fa6b0614636335b
NolanNtrg <notargiacomo.nolan07@gmail.com>
2026-02-20 15:35:13 +0100

    resume changes

--- src/components/terminal/Screen.tsx
+++ src/components/terminal/Screen.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 import { useTranslation } from "react-i18next";
-import { ProjectsContainer } from "./ProjectsContainer.tsx";
+import { ProjectsContainer } from "./ProjectsContainer";
 
 export type Project = {
   title: string;
