11b02f3610cb1e72d55183832111b321fab8a4b2
NolanNtrg <notargiacomo.nolan07@gmail.com>
2026-03-02 13:50:29 +0100

    svg fix + accessibility

--- src/components/hero/ToggleTheme.tsx
+++ src/components/hero/ToggleTheme.tsx
@@ -18,15 +18,17 @@ const ThemeToggle = () => {
 
   return (
     <div className="flex items-center gap-2 text-(--color-text)">
-      <Icon src="sun.svg" />
+      <Icon src="/sun.svg" />
       <Switch
         checked={isDarkTheme}
         onChange={() => setDarkTheme(!isDarkTheme)}
         className="cursor-pointer group inline-flex h-6 w-11 items-center rounded-full bg-gray-200 transition data-checked:bg-(--green)"
+        aria-label="Bouton de changement de thème du site : clair ou sombre."
+        aria-labelledby="theme-toggle"
       >
         <span className="size-4 translate-x-1 rounded-full bg-white transition group-data-checked:translate-x-6" />
       </Switch>
-      <Icon src="moon.svg" />
+      <Icon src="/moon.svg" />
     </div>
   );
 };
